31acfb2558dbdd9786a82e47c5bb8f82eb5e13cd
Jason-Donmoyer <j.donmoyer523@gmail.com>
2021-10-19 17:53:35 -0400

    HTML, CSS, JS: -HTML: --added div to display photos -CSS: --styled image-container -JS: --set up apiUrl and apiKey for Unsplash API --set up async func for fetching data from api

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Logging it');
+// Unsplash API
+const photoCount = 10;
+const apiKey = 'zF-QCy7sL3cg4TMr3vuHYjNEaBnFe9-L6ZOjt6dCAN8';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${photoCount}`;
+
+
+// Get photos from Unsplash api
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    // Catch error 
+    console.log(error);
+  }
+}
+
+
+// On Load
+getPhotos();
