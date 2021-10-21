d67dc007df33b3aec5088aeb975dd1cbdb897159
Jason-Donmoyer <j.donmoyer523@gmail.com>
2021-10-21 17:42:16 -0400

    HTML, JS: -HTML: removed hidden attribute from loader -JS : --function to infinite scroll --added loader --added setTimeout func to pause load

--- script.js
+++ script.js
@@ -2,14 +2,26 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
-// Photo array
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 
 // Unsplash API
-const photoCount = 10;
+const photoCount = 30;
 const apiKey = 'zF-QCy7sL3cg4TMr3vuHYjNEaBnFe9-L6ZOjt6dCAN8';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${photoCount}`;
+
+
+// Check if images were loaded
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+  }
+}
 
 
 // Helper function to set attributes on photos
@@ -22,6 +34,8 @@ function setAttributes(ele, attributes) {
 
 // Create elements for links and photos and add to DOM
 function displayPhotos() {
+  totalImages = photosArray.length;
+  console.log('total images =', totalImages);
   // Run forEach func to grab data from API call
   photosArray.forEach((photo) => {
     // Create link to Unsplash 
@@ -37,6 +51,8 @@ function displayPhotos() {
       alt: photo.description,
       title: photo.description,
     });
+    // Event listener to check when all photos are loaded
+    img.addEventListener('load', imageLoaded);
     // Put img inside ancvhor element, then both into image-container
     item.appendChild(img);
     imageContainer.appendChild(item);
@@ -57,6 +73,17 @@ async function getPhotos() {
   }
 }
 
+// Check to see if scrolling near bottom of page, load more photos if true
+window.addEventListener('scroll', () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+    ready = false;
+    imagesLoaded = 0; // Any reason to put this into the getPhotos function?
+    getPhotos();
+  }
+});
+
 
 // On Load
-getPhotos();
+setTimeout(() => {
+  getPhotos();
+}, 2000);
